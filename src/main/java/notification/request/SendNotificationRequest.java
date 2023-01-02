ffbe0649dbef10280ebac042972ae9a8e4892796
Khushhal Reddy <reddykhushhal2405@gmail.com>
2023-01-03 03:35:57 +0530

    added push notification

--- src/main/java/notification/request/SendNotificationRequest.java
+++ src/main/java/notification/request/SendNotificationRequest.java
@@ -2,14 +2,27 @@ package notification.request;
 
 import lombok.Data;
 
+import java.util.Map;
+
 @Data
 public class SendNotificationRequest {
 
     private String channelType;
 
+    // Push and Email
+    private String subject;
+
     //Email
     private String emailId;
-    private String subject;
     private String body;
+
+    //Push
+    private String content;
+    private String topic;
+    private String title;
+    private String message;
+    private Map<String, String> data;
+    private String image;
+    private String token;
 
 }
--- src/main/java/notification/service/impl/PushNotificationServiceImpl.java
+++ src/main/java/notification/service/impl/PushNotificationServiceImpl.java
@@ -1,12 +1,23 @@
 package notification.service.impl;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+import lombok.extern.slf4j.Slf4j;
 import notification.request.SendNotificationRequest;
 import notification.response.SendNotificationResponse;
 import notification.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("push")
+@Slf4j
 public class PushNotificationServiceImpl implements NotificationService {
+
+    @Autowired
+    private FirebaseMessaging firebaseMessaging;
+
     @Override
     public String getType() {
         return "push";
@@ -14,8 +25,28 @@ public class PushNotificationServiceImpl implements NotificationService {
 
     @Override
     public SendNotificationResponse send(SendNotificationRequest request) {
-        SendNotificationResponse response = new SendNotificationResponse();
-        System.out.println("Push Notification Working");
+        SendNotificationResponse response;
+        try{
+            Notification notification = Notification
+                    .builder()
+                    .setTitle(request.getSubject())
+                    .setBody(request.getContent())
+                    .build();
+
+            Message message = Message
+                    .builder()
+                    .setToken(request.getToken())
+                    .setNotification(notification)
+                    .putAllData(request.getData())
+                    .build();
+
+            String result = firebaseMessaging.send(message);
+            log.info("Response from firebaseMessaging : {}",result);
+            response = new SendNotificationResponse(200, "SUCCESS");
+        } catch (Exception e) {
+            log.info("Exception occurred in Sending Push Notification!!!!");
+            response = new SendNotificationResponse(500, "FAILURE", e.getMessage());
+        }
         return response;
     }
 }
